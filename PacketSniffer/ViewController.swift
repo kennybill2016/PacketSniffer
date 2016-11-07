99c0e100639a3503c2ce16688627bc1c706248b0
weihu <305977296@qq.com>
2016-11-07 17:09:56 +0800

    add file

--- PacketSniffer/ViewController.swift
+++ PacketSniffer/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+private let kRecentRequestCellIdentifier = "recentRequests"
+private let kRecentRequestCachedIdentifier = "requestsCached"
 
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.title = "App Name".localized()
+        do {
+            try Manager.sharedManager.setup()
+        }catch {
+            NSLog("Fail to setup manager")
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: - TableView DataSource & Delegate
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: kRecentRequestCellIdentifier, for: indexPath) as! RecentRequestsCell
+        cell.setCellInfo(indexPath: indexPath as NSIndexPath)
+        return cell
+    }
+    
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 3;
+    }
+    
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat{
+        guard indexPath.row != 2 else {
+            return 60
+        }
+        return 43
+    }
+    
 }
+
 
