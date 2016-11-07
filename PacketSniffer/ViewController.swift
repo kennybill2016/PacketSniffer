41ba8c69d78d2f02be7318fe34dedd5512c581c1
weihu <305977296@qq.com>
2016-11-07 16:15:10 +0800

    create project

---
+++ PacketSniffer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PacketSniffer
+//
+//  Created by lijinwei on 2016/11/7.
+//  Copyright © 2016年 ljw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
