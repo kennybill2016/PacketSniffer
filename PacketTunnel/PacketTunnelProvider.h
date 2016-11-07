fdec46006d20c62ad18a7af65d2f171369bcc80a
weihu <305977296@qq.com>
2016-11-07 17:25:42 +0800

    add tunnel target

---
+++ PacketTunnel/PacketTunnelProvider.h
@@ -0,0 +1,13 @@
+//
+//  PacketTunnelProvider.h
+//  PacketTunnel
+//
+//  Created by lijinwei on 2016/11/7.
+//  Copyright © 2016年 ljw. All rights reserved.
+//
+
+@import NetworkExtension;
+
+@interface PacketTunnelProvider : NEPacketTunnelProvider
+
+@end
